6d9ba2e9d804c7f35fb6acfcfa3283d173965652
Wok <woctezuma@users.noreply.github.com>
2019-12-23 11:58:29 +0100

    Package set-up

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+# noinspection PyUnresolvedReferences
+import setuptools
+
+setup(
+    name='Google-Colab-Transfer',
+    packages=['google-colab-transfer'],
+    install_requires=[
+    ],
+    version='0.1.0',
+    description='Transfer data between Colab and Drive.',
+    long_description='Transfer data between Colab and Drive.',
+    long_description_content_type='text/x-rst',
+    author='Wok',
+    author_email='wok@tuta.io',
+    url='https://github.com/woctezuma/google-colab-transfer',
+    download_url='https://github.com/woctezuma/google-colab-transfer/archive/0.1.0.tar.gz',
+    keywords=['google-colab', 'google-colaboratory', 'google-drive'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
