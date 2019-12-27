0f6c2f38b1398165561286d0de1c0c43dedd028d
Wok <woctezuma@users.noreply.github.com>
2019-12-27 20:09:25 +0100

    Export copy_folder() as an alternative to copy_folder_structure() with different function arguments

--- colab_transfer/__init__.py
+++ colab_transfer/__init__.py
@@ -1,4 +1,3 @@
-
-from .utils import get_path_to_home_of_google_drive, get_path_to_home_of_local_machine
 from .google_drive import mount_google_drive, is_google_drive_mounted
-from .transfer import  copy_file, copy_folder_structure
+from .transfer import copy_file, copy_folder, copy_folder_structure
+from .utils import get_path_to_home_of_google_drive, get_path_to_home_of_local_machine
